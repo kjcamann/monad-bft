08656290bf30a3a27b2554a925eb022378031f8f
Andre Benedito <38482987+andr-dev@users.noreply.github.com>
2025-08-07 11:47:39 -0400

    Expose event ring descriptor info

--- monad-event-ring/src/descriptor/mod.rs
+++ monad-event-ring/src/descriptor/mod.rs
@@ -42,6 +42,11 @@ where
         }
     }
 
+    /// Produces the [`EventDescriptorInfo`] associated with this descriptor.
+    pub fn info(&self) -> EventDescriptorInfo<D> {
+        EventDescriptorInfo::new(self.raw.info())
+    }
+
     /// Attempts to read the payload associated with this event descriptor as the associated
     /// [`T::Event`](EventDecoder::Event) type.
     pub fn try_read(&self) -> EventPayloadResult<D::Event> {
@@ -123,6 +128,9 @@ where
     /// See [`EventDecoder`] for more details.
     pub event_type: u16,
 
+    /// The time at which the event was recorded.
+    pub record_epoch_nanos: u64,
+
     /// The flow information associated with this event descriptor,
     ///
     /// See [`EventDecoder::FlowInfo`] for more details.
@@ -137,6 +145,7 @@ where
         Self {
             seqno: raw.seqno,
             event_type: raw.event_type,
+            record_epoch_nanos: raw.record_epoch_nanos,
             flow_info: D::transmute_flow_info(raw.content_ext),
         }
     }

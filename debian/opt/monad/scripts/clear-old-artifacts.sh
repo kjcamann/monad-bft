768d6dccbe2a18cb3c46aa1f95fd88a1ad642c5b
Ken Camann <kcamann@category.xyz>
2025-11-18 12:13:01 -0500

    [systemd] add blockcapd service script
    Also make clear-old-artifacts.sh clean up the BLOCKCAPD_ROOT_DIR
    directory

--- debian/opt/monad/scripts/clear-old-artifacts.sh
+++ debian/opt/monad/scripts/clear-old-artifacts.sh
@@ -23,4 +23,23 @@ if [ -n "$NEW_FILES" ]; then
 else
     echo "No new files detected. Skipping deletion of ledger files."
 fi
+
+# When monad-blockcapd is running, BLOCKCAPD_ROOT_DIR and BLOCKCAPD_MAX_GB
+# should be set in ~monad/.env (or otherwise somehow injected into the environment
+# of this script). If they are not defined, this section does not run. If they
+# are both defined (and BLOCKCAPD_MAX_GB is not zero), it deletes the oldest files
+# in the local block archive until the maximum size (in GiB) is less than
+# BLOCKCAPD_MAX_GB
+if [ -d "$BLOCKCAPD_ROOT_DIR" ] && [ "${BLOCKCAPD_MAX_GB:-0}" -ne "0" ]; then
+  BLOCKCAPD_MAX_BYTES=$((BLOCKCAPD_MAX_GB * 1024 * 1024 * 1024))
+
+  # while the total size of the archive is greater than allowed, delete the
+  # oldest subdirectory (group of 10,000 capture files)
+  while [ "$(du -sb "$BLOCKCAPD_ROOT_DIR" | awk '{print $1}')" -gt "$BLOCKCAPD_MAX_BYTES" ]; do
+    OLDEST_SUBDIR=$(find "$BLOCKCAPD_ROOT_DIR" -mindepth 1 -maxdepth 1 -type d -printf '%T@ %p\n' \
+           | sort -n | head -1 | cut -d' ' -f2-)
+    [ -n "$OLDEST_SUBDIR" ] && rm -rf "$OLDEST_SUBDIR" || break
+  done
+fi
+
 exit 0
